afd02d52a3584782962c82f3061c8c8db607eca2
Cole Roberts <cole.alan.roberts@gmail.com>
2015-09-30 23:54:28 -0500

    UI improvements and inject a div programmatically

---
+++ build/dialog.js
@@ -0,0 +1,7 @@
+function createDialog() {
+	var div = document.createElement('div');
+	document.body.appendChild(div);
+
+	// Set class
+	div.classList.add('ernest-dialog');
+}
---
+++ build/min/dialog-min.js
@@ -0,0 +1,1 @@
+function createDialog(){var e=document.createElement("div");document.body.appendChild(e),e.classList.add("ernest-dialog")}
